2f5a0f70f7fbea444cc3b83dd32497ce3994d826
m0nk3ybraindead <zyphr.euch@gmail.com>
2022-01-03 21:46:41 +0800

    made navbar

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,26 @@
 from src import app
 from flask import render_template
 
+
 @app.route("/")
+@app.route("/home/")
 def home():
+    return render_template('home.html')
+
+@app.route("/intro/")
+def intro():
+    return render_template('home.html')
+
+@app.route("/topic/")
+def topic():
+    return render_template('home.html')
+
+# basically the whole hata website
+@app.route("/ref/")
+def ref():
+    return render_template('home.html')
+
+# meta
+@app.route("/meta/")
+def meta():
     return render_template('home.html')
